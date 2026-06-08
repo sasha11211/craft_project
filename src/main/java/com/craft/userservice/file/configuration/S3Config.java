cff1f4c9ccd6c5510cfef242261cdf7a5d3f2b78
Aleksandr Kochin <123392016+sasha11211@users.noreply.github.com>
2026-06-08 21:14:51 +0300

    Add S3 presigner bean

--- src/main/java/com/craft/userservice/file/configuration/S3Config.java
+++ src/main/java/com/craft/userservice/file/configuration/S3Config.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 @EnableConfigurationProperties(S3Properties.class)
@@ -14,6 +15,14 @@ public class S3Config {
     @Bean
     public S3Client s3Client(S3Properties s3Properties) {
         return S3Client.builder()
+                .region(Region.of(s3Properties.getRegion()))
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner(S3Properties s3Properties) {
+        return S3Presigner.builder()
                 .region(Region.of(s3Properties.getRegion()))
                 .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
