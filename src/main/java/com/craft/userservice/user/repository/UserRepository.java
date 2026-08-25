68d94dcbef34c9172cf393bcdf0b0477bebf2555
sasha11211 <kochinaleksandr@gmail.com>
2026-08-25 18:40:31 +0300

    fix model_2

--- src/main/java/com/craft/userservice/user/repository/UserRepository.java
+++ src/main/java/com/craft/userservice/user/repository/UserRepository.java
@@ -10,10 +10,8 @@ import com.craft.userservice.user.model.User;
 public interface UserRepository extends MongoRepository<User, String>{
 	Optional<User> findByEmail(String email);
 	Optional<User> findByMobile(String mobile);
-	Optional<User> findByUserName(String userName);
 	
 	boolean existsByEmail(String emString);
 	boolean existsByMobile(String mobile);
-	boolean existsByUserName(String userName);
 
 }
